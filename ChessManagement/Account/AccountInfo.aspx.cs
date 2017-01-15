8ecfe8922bf96b24b144f7e9a40e24d76dc705df
Garrett Martin <garrett.atoms@Gmail.com>
2017-01-15 13:13:30 -0500

    Added to AccountInfo and more
    Changed minimum password length to 8, removed phone authentication
    because it's not needed and we don't have a SMS server. Made Account
    info visible when logged-in only, and added some info to the page, but
    don't know how to get the real data in. Someone just has to add the data
    bindings to grab the data.

--- ChessManagement/Account/AccountInfo.aspx.cs
+++ ChessManagement/Account/AccountInfo.aspx.cs
@@ -11,6 +11,14 @@ namespace ChessManagement.Account
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            labelDivision.Text = "First division"; // Set this to the data in the database for the divison string 
+                                                   // (String becasue the division name can be set to anything.) 
+            labelPoints.Text = 100.ToString();     // I didn't know that would work... but this needs to be set as well.
+            int ranking = 1;                       // Set this from database
+            int membersInDivision = 11;            // Set this from database too
+            labelRanking.Text = String.Format("#{0} out of {1}", ranking.ToString(), membersInDivision.ToString());
+            tableMatches.GridLines = GridLines.Both;
+            tableMatches.DataBind();    // You have to define the data from the DB that will import the match data. I don't know how to do that.
 
         }
     }
